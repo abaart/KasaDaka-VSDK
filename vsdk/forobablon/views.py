3c2ba3993e8b3d29fb9ecbaa989c13bfcbd27439
Pjotr <pjotrscholtze1@gmail.com>
2020-05-15 17:27:49 +0200

    debug info

--- vsdk/forobablon/views.py
+++ vsdk/forobablon/views.py
@@ -6,6 +6,24 @@ from django.conf import settings
 
 from vsdk.service_development.models import ChoiceSaved, CallSession, SpokenUserInput, SpokenUserInput
 
+class Statistics:
+  def __init__(self):
+    self._options = {}
+
+  def register_option(self, name: str):
+    if name not in self._options:
+      self._options[name] = 0
+
+    self._options[name] += 1
+
+  @property
+  def __dict__(self):
+    return {
+      "options": list(self._options.keys()),
+      "results": list(self._options.values()),
+    }
+
+
 class Result:
   def __init__(self, session_id, phone_number: str, start_time: str, end_time: str):
     self.session_id = session_id
@@ -26,6 +44,8 @@ class Result:
   @property
   def finished(self) -> bool:
     return self.language is not None and self.answer is not None
+
+
 def _get_results():
   result_object = {}
   all_sessions = CallSession.objects.all().order_by("pk")
@@ -52,8 +72,15 @@ def _get_results():
     obj.append(result_object[k])
   return reversed(obj)
 
+
 def results(request):
   obj = _get_results()
+  languages = Statistics()
+  options = Statistics()
+
+  for item in obj:
+    languages.register_option(item.language)
+    options.register_option(item.answer)
 
   yesNoResults = ChoiceSaved.yes_no_objects.all()
   test_path = settings.MEDIA_URL + "uploads/*"
@@ -65,7 +92,9 @@ def results(request):
     'test_path': glob.glob(test_path),
     'test_path2': glob.glob(test_path2),
     'test_path3': test_path3,
-    'dump': obj
+    'dump': obj,
+    "stats_language": json.dumps(languages.__dict__),
+    "stats_options": json.dumps(languages.__dict__),
   }
 
   return render(request, 'results.html', context=context)
